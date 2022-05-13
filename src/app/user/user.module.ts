4042c3a67c19f4cfcb177981aa5f8edd4b026b56
Jose Alfredo Tapia Hernandez <e-jatapia@neoris.com>
2022-05-13 15:12:04 -0500

    Creating a reusable modal

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
-
+import { SharedModule } from '../shared/shared.module'
 
 
 @NgModule({
@@ -9,8 +9,12 @@ import { AuthModalComponent } from './auth-modal/auth-modal.component';
     AuthModalComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
+  // Everytime that we create a new modal, all the new 
+  // components related to the modal needs to be exported to 
+  // the app.module.ts:
   exports: [
     AuthModalComponent
   ]
