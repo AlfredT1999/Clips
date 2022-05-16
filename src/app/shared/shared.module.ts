cd73e24fdd35aa3dde4eb58085c2fc904fc50471
Jose Alfredo Tapia Hernandez <e-jatapia@neoris.com>
2022-05-16 16:06:13 -0500

    Creating input component and validation errors were added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
 import { TabsContainerComponent } from './tabs-container/tabs-container.component';
 import { TabComponent } from './tab/tab.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
     ModalComponent,
     TabsContainerComponent,
-    TabComponent
+    TabComponent,
+    InputComponent
   ],
   imports: [
     CommonModule
