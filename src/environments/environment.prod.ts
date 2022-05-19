6e9d6c36cce961ee40d6169365b8301d657efc74
Jose Alfredo Tapia Hernandez <e-jatapia@neoris.com>
2022-05-19 12:25:18 -0500

    Importing AngularFire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBR0UcRbd6d4ooUCo7ZxrdXmkJGeMmB69w",
+    authDomain: "clips-8a4bd.firebaseapp.com",
+    projectId: "clips-8a4bd",
+    storageBucket: "clips-8a4bd.appspot.com",
+    messagingSenderId: "770654285490",
+    appId: "1:770654285490:web:cab9a03931a1911e1763ab"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBR0UcRbd6d4ooUCo7ZxrdXmkJGeMmB69w",
+    authDomain: "clips-8a4bd.firebaseapp.com",
+    projectId: "clips-8a4bd",
+    storageBucket: "clips-8a4bd.appspot.com",
+    messagingSenderId: "770654285490",
+    appId: "1:770654285490:web:cab9a03931a1911e1763ab"
+  }
 };
 
 /*
