2777be762f17c79c3e6513a9f26d61a2f837ae42
Jose Alfredo Tapia Hernandez <e-jatapia@neoris.com>
2022-05-19 14:22:30 -0500

    Auth service added

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -49,10 +48,7 @@ export class RegisterComponent {
   alertMsg = 'Please wait! Your account is being created.'
   inSubmission = false
 
-  constructor(
-    private auth: AngularFireAuth,
-    private db: AngularFirestore
-  ) {}
+  constructor(private auth: AuthService) {}
 
   async register() {
     this.showAlert = true
@@ -60,17 +56,8 @@ export class RegisterComponent {
     this.alertMsg = 'Please wait! Your account is being created.'
     this.inSubmission = true
 
-    const { email, password } = this.registerForm.value
-
     try {
-      const userCredentials = await this.auth.createUserWithEmailAndPassword(email, password)
-      
-      await this.db.collection('users').add({
-        name: this.name.value,
-        email: this.email.value,
-        age: this.age.value,
-        phoneNumber: this.phoneNumber.value
-      })
+      await this.auth.createUser(this.registerForm.value)
     } 
     catch (error) {
       console.error(error)
