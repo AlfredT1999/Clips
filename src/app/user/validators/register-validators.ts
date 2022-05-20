cb459a0800034ac77897a9a87f75a183b46421ce
Jose Alfredo Tapia Hernandez <e-jatapia@neoris.com>
2022-05-20 10:56:37 -0500

    Refactoring register validator with factory func.

--- src/app/user/validators/register-validators.ts
+++ src/app/user/validators/register-validators.ts
@@ -1,15 +1,19 @@
-import { AbstractControl, ValidationErrors } from "@angular/forms";
+import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
 
 export class RegisterValidators {
+    
     // A static function is a utility function:
-    static match(group: AbstractControl): ValidationErrors | null {
-        const control = group.get('password')
-        const matchingControl = group.get('confirm_password')
-
-        if(!control || ! matchingControl) return { controlNotFound: false }
-
-        const error = control.value === matchingControl ? null : { noMatch: true }
-
-        return error
+    static match(controlName: string, matchingControlName: string): ValidatorFn {
+        // Factory function:
+        return (group: AbstractControl): ValidationErrors | null => {
+            const control = group.get('password')
+            const matchingControl = group.get('confirm_password')
+    
+            if(!control || ! matchingControl) return { controlNotFound: false }
+    
+            const error = control.value === matchingControl.value ? null : { noMatch: true }
+    
+            return error
+        }
     }
 }
