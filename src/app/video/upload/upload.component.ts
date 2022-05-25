cb9a7840c56ea8f41e6d5eeeff4b6853404a4605
Jose Alfredo Tapia Hernandez <e-jatapia@neoris.com>
2022-05-25 13:20:35 -0500

    Add timestamp

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -6,6 +6,7 @@ import { v4 as uuid } from 'uuid'
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import firebase from 'firebase/compat/app';
 import { ClipService } from 'src/app/services/clip.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-upload',
@@ -39,7 +40,8 @@ export class UploadComponent implements OnDestroy {
   constructor(
     private storage: AngularFireStorage, 
     private auth: AngularFireAuth,
-    private clipsService: ClipService
+    private clipsService: ClipService,
+    private router: Router
   ) 
   { 
     auth.user.subscribe(user => this.user = user)
@@ -86,22 +88,29 @@ export class UploadComponent implements OnDestroy {
       last(),
       switchMap(() => clipRef.getDownloadURL())
     ).subscribe({
-      next: (url) => {
+      next: async (url) => {
         const clip = {
           uid: this.user?.uid as string,
           displayName: this.user?.displayName as string,
           title: this.title.value,
           fileName: `${clipFileName}.mp4`,
-          url
+          url,
+          timestamp: firebase.firestore.FieldValue.serverTimestamp()
         }
 
-        this.clipsService.createClip(clip)
+        const clipDocRef = await this.clipsService.createClip(clip)
 
         console.log(clip)	
 
         this.alertColor = 'green'
         this.alertMsg = 'Success! Your clip is now ready to share with the world!'
         this.showPercentage = false
+
+        setTimeout(() => {
+          this.router.navigate([
+            'clip', clipDocRef.id
+          ])
+        }, 1000)
       },
       error: (error) => {
         this.uploadForm.enable()
